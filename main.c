35cf1c220c429c905171290c585680bef5592ae9
alexisGalarzaSustaita <a355316@alumnos.uaslp.mx>
2024-10-14 16:48:17 +0000

    Cambios en los main

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <main.h>
 
 int main(){
-    printf("Hello world\n"); 
+    
+    FILE *grammar=fopen("gramatica.txt","r"); 
+    if(grammar==NULL){
+        perror("Error opennig the file"); 
+        return EXIT_FAILURE; 
+    }
+
+    Node *head=createLinkedList(grammar); 
+
+    fclose(grammar); 
+
+    printList(head);
+
+    freeLinkedList(head); 
 
     return EXIT_SUCCESS; 
 }
--- main.h
+++ main.h
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAX_LINE_LENGTH 100
+
+typedef struct Node
+{
+    char *line; 
+    struct Node *next; 
+}; 
+
+Node* createNode(const char *line); 
+void appendNode(Node **head, const char *line); 
+void freeLinkedList(Node *head);
+Node* createLinkedList(FILE *file); 
+void printfList(Node *head);  
